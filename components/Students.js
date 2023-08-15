fe3d77b9b44b35034529708c4e2b71921506e94e
jebaraj-john <jeba.j@live.com>
2023-08-15 16:06:17 +0530

    19 implement role based filtering on the attendance page (#22)
    * attendance filters added
    
    * attendance filter moved new component

--- components/Students.js
+++ components/Students.js
@@ -9,8 +9,8 @@ import {
 } from 'react-native';
 
 import { StudentsStyles } from '../themes/default';
-import { Button, Text, SegmentedButtons } from 'react-native-paper';
-import {urls} from "../config";
+import { ActivityIndicator, Text, SegmentedButtons } from 'react-native-paper';
+import {GetStudents} from "../services/services.js"
 
 
 const AttendanceButton = (props) => {
@@ -41,85 +41,46 @@ const AttendanceButton = (props) => {
 };
 
 const StudentsList = (props) => {
-    const [data, setData] = React.useState([])
-
-    const get_attendance_url = `${urls.attendance_url}?type=get_attendance&email=${props.teacherEmail}`
-    console.log(get_attendance_url)
+    const [data, setData] = React.useState([]);
+    const [isLoading, setLoader] = React.useState(false);
     React.useEffect(() => {
         async function fetchMyAPI() {
             try {
-                let response = await fetch(get_attendance_url)
-                let stud_list = await response.json();
-                console.log(stud_list);
-                setData(stud_list);
+                setLoader(true);
+                let studList = await GetStudents(props.teacherInfo.email, props.center);
+                setData(studList);
+                console.log(studList);
             }
             catch(error) {
                 console.log(error);
             }
+            finally {
+                setLoader(false);
+            }
         }
 
-        fetchMyAPI()
-    }, []);
-
-    const createAttendanceData = (attRecords, teacherEmail, date=null) => {
-        let attendanceData = {
-            "type": "update_attendance",
-            "teacher_email": teacherEmail,
-            "att_data": [],
-        };
-
-        if(date) {
-            attendanceData["date"] = date;
+        if (props.teacherInfo.email) {
+            fetchMyAPI()
         }
 
-        attRecords.forEach(attRecord => {
-            attendanceData.att_data.push({
-                "id": attRecord.id,
-                "att": attRecord.att,
-                "name": attRecord.name,
-            });
-        });
-
-        return attendanceData;
-    };
-
-    const submitAtt = (e) => {
-        const attRecords = createAttendanceData(data, props.teacherEmail)
-        console.log(attRecords)
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(attRecords)
-        };
-        const update_attendance_url = `${urls.attendance_url}`
-        fetch(update_attendance_url, requestOptions)
-          .then(response => response.json())
-          .then(data =>  {
-            Alert.alert('', 'Attendance submitted!', [
-                { text: 'OK', onPress: () => console.log('OK Pressed') },
-            ]);
-        });
-
-    };
+    }, [props.teacherInfo]);
 
 
     const Item = ({item}) => (
         <View style={StudentsStyles.item}>
             <Text style={StudentsStyles.title}>{item.name}</Text>
-            <AttendanceButton defaultValue={item.att} onValueChange={(value) => {item.att = value;}} />
+            <AttendanceButton defaultValue={item.att} onValueChange={(value) => {item.att = value; props.onValueChange(data)}} />
         </View>
     );
 
     return (
         <SafeAreaView style={StudentsStyles.container}>
+            <ActivityIndicator animating={isLoading} />
             <FlatList
                 data={data}
                 renderItem={({ item }) => <Item item={item} />}
                 keyExtractor={item => item.id}
             />
-            <Button mode="contained-tonal" name="submit" key="submit" onPress={submitAtt.bind(this)} >
-                <Text >Submit</Text>
-            </Button>
         </SafeAreaView>
     );
 };
--- services/services.js
+++ services/services.js
@@ -1,4 +1,5 @@
 import { events } from "../data/events_data";
+import {urls} from "../config";
 
 export function getEvent() {
   const eventList = events;
@@ -6,6 +7,31 @@ export function getEvent() {
 }
 
 export function filterEvent(eventLabel) {
-  let filtredEvent = getEvent().filter(type => type.label === eventLabel);
-  return filtredEvent;
+  let filteredEvent = getEvent().filter(type => type.label === eventLabel);
+  return filteredEvent;
+}
+
+export async function GetStudents(email, center) {
+  const get_attendance_url = `${urls.attendance_url}?type=get_attendance&email=${email}&center=${center}`
+  try {
+      let response = await fetch(get_attendance_url)
+      return await response.json();
+
+  }
+  catch(error) {
+      console.log(error);
+  }
+}
+
+
+export async function PostAttendance(attRecords) {
+    const update_attendance_url = `${urls.attendance_url}`
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(attRecords)
+    };
+    let response = await fetch(update_attendance_url, requestOptions);
+    return await response.json();
+
 }
