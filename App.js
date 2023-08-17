06384c49a44945b55b42478df40b33fa3b4efe27
Jebaraj John <jeba.j@live.com>
2023-08-17 21:44:31 +0530

    after login hooks installed

--- App.js
+++ App.js
@@ -19,9 +19,32 @@ const theme = {
     },
 };
 
+
+
 export default function Main() {
+<<<<<<< HEAD
     const [index, setIndex] = React.useState(0);
+    const [isLoggedIn, setLoginStatus] = React.useState(false);
     const [userInfo, setUserInfo] = React.useState(null);
+
+    const RenderPage = () => {
+        if (!isLoggedIn) {
+          return (
+            <LoginScreen afterSignIn={(userInfo)=> {setLoginStatus(true)}}/>
+          )
+        }
+        else {
+          return(
+            userInfo && (
+                <BottomNavigation
+                    navigationState={{ index, routes }}
+                    onIndexChange={setIndex}
+                    renderScene={renderScene}
+                />
+            )
+          )
+        }
+      }
 
     const [routes] = React.useState([
         {
@@ -82,15 +105,51 @@ export default function Main() {
     });
     return (
         <PaperProvider theme={theme}>
-            {userInfo && (
-                <BottomNavigation
-                    navigationState={{ index, routes }}
-                    onIndexChange={setIndex}
-                    renderScene={renderScene}
-                />
-            )}
+            {RenderPage()}
         </PaperProvider>
     );
+=======
+  const [index, setIndex] = React.useState(0);
+  const [isLoggedIn, setLoginStatus] = React.useState(false);
+
+  const [routes] = React.useState([
+    { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline'},
+    { key: 'reports', title: 'Reports', focusedIcon: 'file-document-multiple',unfocusedIcon:"file-document-multiple-outline"},
+    { key: 'events', title: 'Events', focusedIcon: 'calendar-month',unfocusedIcon:"calendar-month-outline" },
+  ]);
+  const RenderPage = () => {
+    if (!isLoggedIn) {
+      return (
+        <LoginScreen afterSignIn={(userInfo)=> {setLoginStatus(true)}}/>
+      )
+    }
+    else {
+      return(
+        <BottomNavigation navigationState={{ index, routes }} onIndexChange={setIndex} renderScene={renderScene}
+        />
+      )
+    }
+  }
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: () => <HomeScreen centers={["NLAG"]}
+      services={["1st Service", "2nd Service"]} departments={["Beginner", "Primary"]} role="leader"
+      teachers={[{"name": "Sam", "email": "mailx@gmail.com"},{"name": "Daniel", "email": "jsx@live.com"}]}
+      userInfo={{"email": "xyz@gmail.com", "name": "Dave Jones"}}
+    />,
+    reports: () => <Reports centers={["NLAG"]}
+      services={["1st Service", "2nd Service"]} departments={["Beginner", "Primary"]} role="leader"
+      teachers={[{"name": "Sam", "email": "mailx@gmail.com"},{"name": "Daniel", "email": "jsx@live.com"}]}
+      userInfo={{"email": "xyz@gmail.com", "name": "Dave Jones"}}
+    />,
+    events: LoginScreen,
+  });
+  return (
+    <PaperProvider theme={theme}>
+      {RenderPage()}
+    </PaperProvider>
+  );
+>>>>>>> 499661e (after login hooks installed)
 }
 
 AppRegistry.registerComponent(appName, () => Main);
