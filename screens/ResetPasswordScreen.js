641089322a1c583bf5be5dfd65f586b5f609b41b
Jebaraj John <jeba.j@live.com>
2023-09-06 18:51:36 +0530

    reset password fixed

--- screens/ResetPasswordScreen.js
+++ screens/ResetPasswordScreen.js
@@ -4,48 +4,120 @@ import Logo from "../components/Logo";
 import Header from "../components/Header";
 import { Button, TextInput, BackButton } from "../components/Form";
 import { emailValidator } from "../helpers/emailValidator";
+import { supabase } from "../lib/supabase";
+import { AuthContext } from "../User";
 
-export default function ResetPasswordScreen({ navigation }) {
+function ResetPage({onChangeEmail, setPage, navigation}) {
     const [email, setEmail] = useState({ value: "", error: "" });
+    const resetEmail = async (emailId) => {
+        const {error} = await supabase.auth.resetPasswordForEmail(emailId, {
+            redirectTo: '',
+        });
 
-    const sendResetPasswordEmail = () => {
+        if (!error) {
+            onChangeEmail(email);
+        }
+    };
+
+    const sendResetPasswordEmail = async () => {
         const emailError = emailValidator(email.value);
         if (emailError) {
             setEmail({ ...email, error: emailError });
+
             return;
         }
-        //TODO: Integrate to supabse
-        navigation.navigate("LoginScreen");
+        resetEmail(email.value);
+        setPage("updatePassword");
+
+
     };
 
     return (
-        <Background>
-            <BackButton
+        <>
+         <BackButton
                 goBack={() => {
                     navigation.navigate("LoginScreen");
                 }}
             />
             <Logo />
-            <Header headerText="Restore Password"></Header>
+            <Header headerText="Sign In With OTP"></Header>
             <TextInput
                 label="E-mail address"
                 returnKeyType="done"
                 value={email.value}
-                onChangeText={(text) => setEmail({ value: text, error: "" })}
+                onChangeText={(text) => {
+                    setEmail({ value: text, error: "" })
+                }}
                 error={!!email.error}
                 errorText={email.error}
                 autoCapitalize="none"
                 autoCompleteType="email"
                 textContentType="emailAddress"
                 keyboardType="email-address"
-                description="You will receive email with password reset link."
+                description="You will receive an email with code."
             />
             <Button
                 mode="contained"
                 onPress={sendResetPasswordEmail}
                 style={{ marginTop: 16 }}
-                btnText=" Send Instructions"
+                btnText=" Send Code"
             />
+        </>
+    )
+}
+
+function OTPSignInPage({email, navigation}) {
+    const [code, setCode] = useState("")
+    const { signInWithOTP } = React.useContext(AuthContext);
+
+
+    const doLogin = async () => {
+        signInWithOTP(email, code);
+    };
+
+    return (
+        <>
+         <BackButton
+                goBack={() => {
+                    navigation.navigate("LoginScreen");
+                }}
+            />
+            <Logo />
+            <Header headerText="Sign In With OTP"></Header>
+            <TextInput
+                label="Code"
+                returnKeyType="done"
+                value={code}
+                onChangeText={setCode}
+                autoCapitalize="none"
+                textContentType="oneTimeCode"
+                keyboardType="number-pad"
+                description="auth code from email"
+            />
+            <Button
+                mode="contained"
+                onPress={doLogin}
+                style={{ marginTop: 16 }}
+                btnText="Sign In"
+            />
+        </>
+    )
+}
+
+
+
+
+export default function ResetPasswordScreen({navigation}) {
+    const [page, changePage] = useState("resetPage");
+    const [email, setEmail] = useState("");
+
+
+    return (
+        <Background>
+           {
+           page === "resetPage" ? <ResetPage navigation={navigation} onChangeEmail={setEmail} setPage={changePage} /> :
+           <OTPSignInPage  navigation={navigation} email={email.value}/>
+           }
         </Background>
     );
 }
